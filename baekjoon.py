1f83a86ab585f63bcee6b29d0efa951cebaa90d4
kimmy-o <71263403+kimmy-o@users.noreply.github.com>
2021-11-04 21:24:59 +0900

    Add files via upload

---
+++ baekjoon.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 1-1 : 2557
+print("Hello World!")
+
+
+# In[4]:
+
+
+# 1-2 : 10781
+for i in range(2):
+    print("강한친구 대한육군")
+
+
+# In[12]:
+
+
+# 1-3 : 10171
+print(
+    "\\     /\\""\n" 
+    " )   (  \')""\n" 
+    "(  /   )""\n" 
+     "  \\(___)|""\n"
+         )
+
+
+# In[23]:
+
+
+# 1-5 : 1000
+A ,B = map(int,input().split())
+print(A+B)
+
+
+# In[ ]:
+
+
+
+
